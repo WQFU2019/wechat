c417bf023c0c1163cbfbb2336883d1935cf894fe
WangQiFu <wqf_email@163.com>
2023-02-28 18:34:54 +0800

    提交autoload.php

---
+++ autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+function classLoader($class)
+{
+    $class = str_replace('luweiss\\wechat\\', '', $class);
+    $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    $file = __DIR__ . '/src/' . $path . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }else{
+        echo $file;
+        exit;
+    }
+}
+
+spl_autoload_register('classLoader');
